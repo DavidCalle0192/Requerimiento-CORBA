b9caeea8137c051a9728f28e0ce3d1445d530453
JhonMZ <alejandromelo@unicauca.edu.co>
2020-06-20 18:07:22 -0500

    update sop_corba

---
+++ src/servidorNotificaciones/sop_corba/NotificacionesImpl.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package servidorNotificaciones.sop_corba;
+
+import servidorAlertas.dto.HistorialAlertasDTO;
+
+/**
+ *
+ * @author JhonMZ
+ */
+public class NotificacionesImpl implements NotificacionesOperations{
+
+    public NotificacionesImpl() {
+    }
+
+    
+    @Override
+    public void enviarAlerta(HistorialAlertasDTO objHistorial) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
